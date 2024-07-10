6e0df986af055cce8a61dfa3409e327182f147b4
yewin <yewinaung831@gmail.com>
2024-07-10 22:12:10 +0700

    add scss 7-1 sass architecture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-
+import '@/styles/main.scss';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
